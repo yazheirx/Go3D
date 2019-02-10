e39de3a0e10a34b4de0fafc6e7ca8707ebd8ed8b
Justin G. Cramer <jcramer@proshipinc.com>
2019-02-09 21:11:17 -0600

    Completed sever.js modifications to be in line with Babylon Express Srvr

--- server.js
+++ server.js
@@ -3,16 +3,7 @@ const express = require('express');
 let port = process.env.PORT || 3000;
 let app = express();
 
-app.get('/', (req, res) => {
-     let page = '<html><head><title>Go3D</title></head><body>';
-     page += '<h1>Welcome to the Go3D project</h1>';
-     page += '<p>Thank you for your interest.</p>';
-     page +=
-          '<p>Please go to <a href="https://github.com/yazheirx/Go3D">GitHub</a> if you want to know more or contribute</p>';
-     page += '</body></html>';
-     res.send(page);
-});
-
 app.listen(port, () => {
      console.log(`Server is up on port ${port}`);
 });
+app.use(express.static('public'));
